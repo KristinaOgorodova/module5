f73313d8a24a965f12cb2d2ebc2d447bfbbe2ebb
Kristina Ogorodova <skrok@inbox.ru>
2022-10-30 20:47:57 +0300

    new

--- index.js
+++ index.js
@@ -3,11 +3,43 @@
 const ads = document.querySelector('.ads');
 ads.remove();
 
-const propList = document.querySelectorAll('.item');
+const book = document.querySelectorAll('.item');
 
+book[0].replaceWith(book[1]);
+book[3].after(book[0]);
+
+const propList = document.querySelectorAll('.props__list');
 console.log(propList);
 
-propList[0].replaceWith(propList[1]);
-propList[4].replaceWith(propList[0]);
+const clone = propList[4].cloneNode(true);
+
+console.log(clone)
+
+propList[4].replaceWith(propList[2]);
+
+
+const two = document.querySelectorAll('.props__item_two');
+console.log(two);
+
+const four = document.querySelectorAll('.props__item_four');
+console.log(four);
+
+four[3].after(four[0])
+two[7].after(two[8])
+two[7].after(two[9])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
