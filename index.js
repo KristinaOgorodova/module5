646da26791a9040860e002899883097b4f0e427a
Kristina Ogorodova <skrok@inbox.ru>
2022-11-01 10:42:26 +0300

    fixing homework

--- index.js
+++ index.js
@@ -8,16 +8,6 @@ const book = document.querySelectorAll('.item');
 book[0].replaceWith(book[1]);
 book[3].after(book[0]);
 
-const propList = document.querySelectorAll('.props__list');
-console.log(propList);
-
-const clone = propList[4].cloneNode(true);
-
-console.log(clone)
-
-propList[4].replaceWith(propList[2]);
-
-
 const two = document.querySelectorAll('.props__item_two');
 console.log(two);
 
@@ -28,7 +18,16 @@ four[3].after(four[0])
 two[7].after(two[8])
 two[7].after(two[9])
 
+const itemsTitle = document.querySelectorAll('.item__title');
+const propsList = document.querySelectorAll('.props__list');
 
+propsList[1].before(itemsTitle[3]);
+propsList[4].before(itemsTitle[4]);
+propsList[5].before(itemsTitle[0]);
+
+console.log(propsList);
+propsList[2].replaceWith(propsList[4]);
+propsList[4].append(propsList[2])
 
 
 
